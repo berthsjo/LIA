8606c49b764d8da6f6f3cf9e818aa3eedf098b0b
Erika Berthsjö <erika@berthsjo.com>
2014-04-04 15:52:54 +0200

    Ändrat
    Ändrat till en helt annan wordpress fil som möjligtvis kan fungera.

--- wordpress/wp-content/themes/shipyard/js/ie.js
+++
@@ -1,8 +0,0 @@
-/*
- * HTML5 support for IE 8 and older browsers
- */
-
-document.createElement('header'); 
-document.createElement('nav'); 
-document.createElement('article'); 
-document.createElement('footer');
